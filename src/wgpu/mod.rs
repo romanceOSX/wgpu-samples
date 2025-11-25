a2ebf876c33664e51ac6c719807de0371e13aea2
romance <106858958+romanceOSX@users.noreply.github.com>
2025-11-24 22:37:41 -0600

    chore: working triangle

--- src/wgpu/mod.rs
+++ src/wgpu/mod.rs
@@ -13,6 +13,42 @@ use winit::{
     window::Window,
 };
 
+use wgpu::util::DeviceExt;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },   
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },   
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },   
+];
+
 // state
 pub struct State {
     surface: wgpu::Surface<'static>,
@@ -21,6 +57,8 @@ pub struct State {
     surface_config: wgpu::SurfaceConfiguration,
     is_surface_configured: bool,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
     window: Arc<Window>,
 }
 
@@ -69,6 +107,14 @@ impl State {
             desired_maximum_frame_latency: 2,
         };
 
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+
         // pipeline
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
@@ -86,7 +132,7 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
-                buffers: &[],
+                buffers: &[Vertex::desc()],
                 compilation_options: wgpu::PipelineCompilationOptions::default() 
             },
             fragment: Some(wgpu::FragmentState {
@@ -125,6 +171,8 @@ impl State {
             surface_config: config,
             is_surface_configured: false,
             render_pipeline: render_pipeline,
+            vertex_buffer: vertex_buffer,
+            num_vertices: VERTICES.len() as u32,
             window: window,
         })
     }
@@ -180,7 +228,9 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+            // here we pass-in the indexes
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.draw(0..self.num_vertices, 0..1);
         }
 
         // submit
