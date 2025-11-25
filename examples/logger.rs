310b7190fa3003a3551b78df1f9cca77ab33d2b9
romance <106858958+romanceOSX@users.noreply.github.com>
2025-11-24 20:43:42 -0600

    chore: logger example

---
+++ examples/logger.rs
@@ -0,0 +1,14 @@
+use log::{debug, error, log_enabled, info, Level};
+
+fn main() {
+    env_logger::init();
+
+    debug!("this is a debug {}", "message");
+    error!("this is printed by default");
+
+    if log_enabled!(Level::Info) {
+        let x = 3 * 4; // expensive computation
+        info!("the answer was: {}", x);
+    }
+}
+
