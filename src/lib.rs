bb2afdc9b763da041f5178c91419f01161049a70
romance <106858958+romanceOSX@users.noreply.github.com>
2026-02-10 22:46:15 -0600

    chore: tutorial spin

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod wgpu;
 pub mod texture;
+pub mod camera;
 
--- src/wgpu/mod.rs
+++ src/wgpu/mod.rs
@@ -20,6 +20,8 @@ use winit::{
 
 use wgpu::util::DeviceExt;
 
+use crate::camera::{Camera, CameraUniform};
+
 const VERTICES: &[Vertex] = &[
     //                  x               y                   z
     Vertex { position: [-0.0868241,     0.49240386,    0.0],     tex_coords: [0.4131759,     1.0 - 0.00759614], }, // A
@@ -63,7 +65,73 @@ impl Vertex {
     }
 }
 
+//
+// Camera Controls
+//
+
+struct CameraController {
+    speed: f32,
+    is_forward_pressed: bool,
+    is_backward_pressed: bool,
+    is_left_pressed: bool,
+    is_right_pressed: bool,
+}
+
+impl CameraController {
+    fn new(speed: f32) -> Self {
+        Self { 
+            speed: speed,
+            is_forward_pressed: false,
+            is_backward_pressed: false,
+            is_left_pressed: false,
+            is_right_pressed: false
+        }
+    }
+
+    fn handle_key(&mut self, code: KeyCode, is_pressed: bool) -> bool {
+        match code {
+            KeyCode::KeyW => {
+                self.is_forward_pressed = is_pressed;
+                true
+            }
+            KeyCode::KeyA => {
+                self.is_left_pressed = is_pressed;
+                true
+            }
+            KeyCode::KeyS => {
+                self.is_backward_pressed = is_pressed;
+                true
+            }
+            KeyCode::KeyD => {
+                self.is_right_pressed = is_pressed;
+                true
+            }
+            _ => false,
+        }
+    }
+
+    fn update_camera(&self, camera: &mut Camera) {
+        use cgmath::InnerSpace;
+        let forward = camera.target - camera.eye;
+        let forward_norm = forward.normalize();
+        let forward_mag = forward.magnitude();
+
+        let right = forward_norm.cross(camera.up);
+        let forward = camera.target - camera.eye;
+        let forward_mag = forward.magnitude();
+
+        if self.is_right_pressed {
+            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+        }
+        if self.is_left_pressed {
+            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+        }
+    }
+}
+
+//
 // state
+//
 pub struct State {
     surface: wgpu::Surface<'static>,
     device: wgpu::Device,
@@ -77,12 +145,18 @@ pub struct State {
     num_indices: u32,
     diffuse_bind_group: wgpu::BindGroup,
     diffuse_texture: crate::texture::Texture,
+    camera: Camera,
+    camera_uniform: CameraUniform,
+    camera_buffer: wgpu::Buffer,
+    camera_bind_group: wgpu::BindGroup,
     window: Arc<Window>,
+    camera_controller: CameraController,
 }
 
 impl State {
     pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
         let size = window.inner_size();
+        let camera_controller = CameraController::new(0.2);
 
         let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor{
             backends: wgpu::Backends::PRIMARY,
@@ -162,7 +236,7 @@ impl State {
             ],
             label: Some("texture_bind_group_layout"),
         });
-        
+
         let diffuse_bind_group = device.create_bind_group(
             &wgpu::BindGroupDescriptor {
                 layout: &texture_bind_group_layout,
@@ -197,7 +271,60 @@ impl State {
         );
         let num_indices = INDICES.len() as u32;
 
+        // camera
+        let camera = Camera {
+            eye: (0.0, 1.0, 2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: config.width as f32 / config.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
+
+        // camera Uniform buffer
+        let mut camera_uniform = CameraUniform::new();
+        camera_uniform.update_view_proj(&camera);
+
+        let camera_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Camera Buffer"),
+                contents: bytemuck::cast_slice(&[camera_uniform]),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            }
+        );
+
+        let camera_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }
+            ],
+            label: Some("camera_bind_group_layout"),
+        });
+
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &camera_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: camera_buffer.as_entire_binding(),
+                }
+            ],
+            label: Some("camera_bind_group"),
+        });
+
+        //
         // pipeline
+        //
+
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
             source: wgpu::ShaderSource::Wgsl(include_str!("../../shaders/shader.wgsl").into()),
@@ -205,7 +332,10 @@ impl State {
 
         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
-            bind_group_layouts: &[&texture_bind_group_layout],
+            bind_group_layouts: &[
+                &texture_bind_group_layout,
+                &camera_bind_group_layout,
+            ],
             push_constant_ranges: &[]
         });
 
@@ -260,7 +390,12 @@ impl State {
             num_indices: num_indices,
             diffuse_bind_group: diffuse_bind_group,
             window: window,
+            camera: camera,
+            camera_uniform: camera_uniform,
+            camera_buffer: camera_buffer,
+            camera_bind_group: camera_bind_group,
             diffuse_texture: diffuse_texture,
+            camera_controller: camera_controller,
         })
     }
 
@@ -315,9 +450,11 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
+
+            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+            render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
 
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
@@ -328,15 +465,18 @@ impl State {
         Ok(())
     }
 
-    fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
-        match (code, is_pressed) {
-            (KeyCode::Escape, true) => event_loop.exit(),
-            _ => {}
+    fn handle_key(&mut self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+        if code == KeyCode::Escape && is_pressed {
+            event_loop.exit();
+        } else {
+            self.camera_controller.handle_key(code, is_pressed);
         }
     }
 
     fn update(&mut self) {
-        // TODO: implement this thang
+        self.camera_controller.update_camera(&mut self.camera);
+        self.camera_uniform.update_view_proj(&self.camera);
+        self.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
     }
 }
 
